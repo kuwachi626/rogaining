0e9fd5c7f3f760e305dda0baeae65f3b9c63f63e
くわっち <137525548+kuwachi626@users.noreply.github.com>
2025-10-15 19:00:00 +0900

    トップページにロゴを追加

--- src/Login.tsx
+++ src/Login.tsx
@@ -33,6 +33,12 @@ export default function Login({ onLogin }: Props) {
 			<Header />
 			<div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
 				<div className="max-w-md w-full space-y-8">
+					<div className="text-center">
+						<img
+							src="./img/logo.png"
+							className="mx-auto h-48 w-auto"
+						/>
+					</div>
 					<div>
 						<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
 							えちぜんロゲピカ大作戦
